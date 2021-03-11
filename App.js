2c99b518d33a628088967a838e06d267e002f492
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-03-12 00:01:31 +0530

    todo app working

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header'
 import TodoItem from './components/TodoItem'
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos,setTodos] = useState([
@@ -16,11 +17,22 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+        setTodos((prevTodos) => {
+            return [
+                {text: text, key: Math.random().toString()},
+                ...prevTodos
+            ]
+        })
+    })
+}
+  
   return (
     <View style={styles.container}>
       < Header />
       <View style={styles.content}>
-        {/* to form */}
+        <AddTodo submitHandler={submitHandler}/>
         <View style={styles.list}>
           <FlatList
             data={todos}
